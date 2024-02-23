69e3d51ff16806760d6b55ff785d3c044622ba4e
Santiago Salazar <santiagosalazar.dev@gmail.com>
2024-02-22 23:41:28 -0400

    Internal pages

---
+++ src/app/portfolio/[projectName]/page.tsx
@@ -0,0 +1,105 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import * as Icon from 'react-feather';
+import FullSection from '@/components/FullSection'
+import Skills from './../../../../public/moks/skills.json';
+import Projects from './../../../../public/moks/portfolio-projects.json';
+
+type Skill = {
+  name: string;
+  url: string;
+  textColor: string;
+  bgColor: string;
+};
+
+type SkillsType = {
+  [key: string]: Skill;
+};
+
+const SkillsData: SkillsType = Skills[0];
+
+// interface ProjectType {
+//   title: string;
+//   description: string;
+//   url: string;
+//   imageRef: string;
+//   tech: string[][];
+// }
+
+const page = ({ params }: { params: any }) => {
+  const pageProject = Projects.find((project: any) => project.id === params.projectName);
+  if (!pageProject) return <div>Proyecto no encontrado</div>;
+  const { title, description, url, repo, imageRef, tech }: any = pageProject;
+
+  const SkillsKeys = Object.keys(SkillsData)
+
+  const technologies = tech.reduce((accumulator: any, currentValue: any) => {
+    if (SkillsKeys.includes(currentValue)) {
+      accumulator.push(SkillsData[currentValue]);
+    }
+    return accumulator;
+  }, [])
+
+  return (
+    <FullSection>
+      <header className='grid gap-20 grid-cols-5'>
+        <div className=" col-span-2">
+          <Image
+            src={imageRef}
+            height="1000"
+            width="1000"
+            className="object-cover rounded-xl py-1"
+            alt={'OnceOnce'}
+          />
+          <Image
+            src={imageRef}
+            height="1000"
+            width="1000"
+            className="object-cover rounded-xl py-1"
+            alt={'OnceOnce'}
+          />
+        </div>
+        <div className="content col-span-3 flex flex-col justify-center">
+          <h1 className="title text-6xl font-bold mb-6">
+            {title}
+          </h1>
+          <p className="description text-ms lg:text-base">
+            {description}
+          </p>
+          <div className="flex my-4">
+            {url && <Link href={url} className='flex mr-4'>Ir al sitio <Icon.ExternalLink className='ml-2' size={24} /></Link>}
+            {repo && <Link href={repo} className='flex mr-4'>Repo <Icon.ExternalLink className='ml-2' size={24} /></Link>}
+          </div>
+          <div className="flex justify-between my-5">
+            <ul className='flex flex-wrap'>
+              {technologies.map((tool: any, index: number) => (
+                <Tag key={index} url={tool.url} bgColor={tool.bgColor} txtColor={tool.textColor}>
+                  {tool.name}
+                </Tag>
+              ))}
+            </ul>
+            <p className="font-bold text-white">
+              Noviembre - 2023
+            </p>
+          </div>
+        </div>
+      </header>
+    </FullSection>
+  )
+}
+
+const Tag = ({ children, bgColor, txtColor, url }: { children: React.ReactNode, bgColor: string, txtColor: string, url: string }) => {
+
+  return (
+    <Link href={url} 
+          className={`mr-3 text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 mb-2 rounded-full`}
+          style={{ backgroundColor: bgColor, color: txtColor }}
+        >
+      {children}
+    </Link>
+  )
+}
+
+
+export default page
--- src/partials/Portfolio.tsx
+++ src/partials/Portfolio.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 import Links from '@/components/Links'
 import FullSection from '@/components/FullSection'
-import Image from 'next/image'
-import Link from 'next/link'
+import Skills from './../../public/moks/skills.json';
 import Projects from './../../public/moks/portfolio-projects.json';
 import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
+
+type Skill = {
+  name: string;
+  url: string;
+  textColor: string;
+  bgColor: string;
+};
+
+type SkillsType = {
+  [key: string]: Skill;
+};
+
+const SkillsData: SkillsType = Skills[0];
 
 const Portfolio = () => {
 
@@ -18,56 +32,69 @@ const Portfolio = () => {
 }
 
 const ProjectCard = () => {
+
+  const SkillsKeys = Object.keys(SkillsData)
+
   return (
     <div className='grid gap-4 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 w-full'>
-      {Projects.map((project, projectKey) => (
-        <CardContainer className="inter-var" key={projectKey}>
-          <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto h-full rounded-xl p-6 border flex flex-col justify-between">
-            <div className="">
-              <CardItem
-                translateZ="50"
-                className="text-xl font-bold text-neutral-600 dark:text-white"
-                  key={`${projectKey}-name`}
-              >
-                {project.title}
-              </CardItem>
-              <CardItem
-                as="p"
-                translateZ="60"
-                className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-                  key={`${projectKey}-description`}
-              >
-                {project.short_description}
-              </CardItem>
-            </div>
-            <div className="">
-              <CardItem translateZ="100" className="w-full mt-4"
-                key={`${projectKey}-image`}>
-                  <Link href={`${project.page || project.url}`}>
-                  <Image
-                    src={project.imageRef}
-                    height="1000"
-                    width="1000"
-                    className="h-50 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-                    alt={project.title}
-                    />
-                  </Link>
-              </CardItem>
-            </div>
-            <ul className='flex justify-between flex-wrap'>
-              {project.links.map((link, index) => (
+      {Projects.map((project, projectKey) => {
+        
+      const listOfSkills = project.tech.reduce((accumulator: Skill[], currentValue: string) => {
+        if (SkillsKeys.includes(currentValue)) {
+          accumulator.push(SkillsData[currentValue]);
+        }
+        return accumulator;
+      }, []);
+
+        return (
+          <CardContainer className="inter-var" key={projectKey}>
+            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto h-full rounded-xl p-6 border flex flex-col justify-between">
+              <div className="">
                 <CardItem
-                  translateZ={20}
-                  className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-                    key={`${index}-stack`}
+                  translateZ="50"
+                  className="text-xl font-bold text-neutral-600 dark:text-white"
+                    key={`${projectKey}-name`}
                 >
-                      <Links href={link[1]} classNames='text-xs' key={index}>{link[0]}</Links>
+                  {project.title}
                 </CardItem>
-                ))}
-              </ul>
-          </CardBody>
-        </CardContainer>
-      ))}
+                <CardItem
+                  as="p"
+                  translateZ="60"
+                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                    key={`${projectKey}-description`}
+                >
+                  {project.short_description}
+                </CardItem>
+              </div>
+              <div className="">
+                <CardItem translateZ="100" className="w-full mt-4"
+                  key={`${projectKey}-image`}>
+                    <Link href={`${project.page}`}>
+                    <Image
+                      src={project.imageRef}
+                      height="1000"
+                      width="1000"
+                      className="h-50 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                      alt={project.title}
+                      />
+                    </Link>
+                </CardItem>
+              </div>
+              <ul className='flex justify-between flex-wrap'>
+                {listOfSkills.map((obj: Skill, index: number) => (
+                  <CardItem
+                    translateZ={20}
+                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                      key={`${index}-stack`}
+                  >
+                        <Links href={obj.url} classNames='text-xs' key={index}>{obj.name}</Links>
+                  </CardItem>
+                  ))}
+                </ul>
+            </CardBody>
+          </CardContainer>
+        )
+      })}
     </div>
   )
 }
