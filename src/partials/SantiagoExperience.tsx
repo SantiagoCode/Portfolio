97931fb4dd203205d686b24ef26cfa40e98c8d92
Santiago Salazar <santiagosalazar.dev@gmail.com>
2024-02-23 01:01:25 -0400

    cleaning repeated code

--- src/partials/SantiagoExperience.tsx
+++ src/partials/SantiagoExperience.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import FullSection from '@/components/FullSection'
+import * as Icon from 'react-feather'
 
 const SantiagoExperience = () => {
   return (
@@ -41,32 +42,29 @@ const ExperienceResume = () => {
 const TimeLine = () => {
   return (
     <div className="col-span-2 space-y-8 relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px xl:before:mx-auto xl:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent">
-        
-        <div className="relative w-full md:w-2/4 xl:w-full flex items-center justify-between xl:justify-normal xl:even:flex-row-reverse xl:even:text-right group is-active">
-            <div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 xl:order-1 xl:group-odd:-translate-x-1/2 xl:group-even:translate-x-1/2">
-                {/* <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="12" height="10">
-                    <path fill-rule="nonzero" d="M10.422 1.257 4.655 7.025 2.553 4.923A.916.916 0 0 0 1.257 6.22l2.75 2.75a.916.916 0 0 0 1.296 0l6.415-6.416a.916.916 0 0 0-1.296-1.296Z" />
-                </svg> */}
-            </div>
-            <div className="w-[calc(100%-4rem)] xl:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow-lg shadow-indigo-700 hover:shadow-indigo-500/50 transition-all duration-300">
-                <time className="font-caveat font-medium text-indigo-500 text-xs">2022 - 2023</time>
-                <div className="font-bold text-slate-900 text-ms">Movidagrafica.co</div>
-                <div className="text-slate-500 text-xs">FullStack developer & Team Leader</div>
-            </div>
-        </div>
+        <Experience date={"2022 - 2023"} agencie={"Movidagrafica.co"} position={"FullStack developer & Team Leader"} />
+        <Experience date={"2022"} agencie={"Radikal Agencies"} position={"FrontEnd developer"} />
+    </div>
+  )
+}
 
-        <div className="relative w-full md:w-2/4 xl:w-full flex items-center justify-between xl:justify-normal xl:even:flex-row-reverse xl:even:text-right group is-active">
-            <div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 xl:order-1 xl:group-odd:-translate-x-1/2 xl:group-even:translate-x-1/2">
-                {/* <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="12" height="10">
-                    <path fill-rule="nonzero" d="M10.422 1.257 4.655 7.025 2.553 4.923A.916.916 0 0 0 1.257 6.22l2.75 2.75a.916.916 0 0 0 1.296 0l6.415-6.416a.916.916 0 0 0-1.296-1.296Z" />
-                </svg> */}
-            </div>
-            <div className="w-[calc(100%-4rem)] xl:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow-lg shadow-indigo-700 hover:shadow-indigo-500/50 transition-all duration-300">
-                <time className="font-caveat font-medium text-indigo-500 text-xs">2022</time>
-                <div className="font-bold text-slate-900 text-ms">Radikal Agencie</div>
-                <div className="text-slate-500 text-xs">FrontEnd developer</div>
-            </div>
-        </div>
+const Experience = ({ date, agencie, position }: { date: string, agencie: string, position: string }) => {
+  return (
+    <div className="relative w-full md:w-2/4 xl:w-full flex items-center justify-between xl:justify-normal xl:even:flex-row-reverse xl:even:text-right group is-active">
+      <div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 xl:order-1 xl:group-odd:-translate-x-1/2 xl:group-even:translate-x-1/2">
+          <Icon.Check size={21} />
+      </div>
+      <div className="w-[calc(100%-4rem)] xl:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow-lg shadow-indigo-700 hover:shadow-indigo-500/50 transition-all duration-300">
+          <time className="font-caveat font-medium text-indigo-500 text-xs">
+            {date}
+          </time>
+          <div className="font-bold text-slate-900 text-ms">
+            {agencie}
+          </div>
+          <div className="text-slate-500 text-xs">
+            {position}
+          </div>
+      </div>
     </div>
   )
 }
