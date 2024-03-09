682ebdcdf26afeabd3a6474751743e827939eda0
Santiago Salazar <santiagosalazar.dev@gmail.com>
2024-03-09 02:34:32 -0400

    Gallery touch

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -5,15 +5,15 @@ const Gallery = ({ project }: { project: any }) => {
   const { ListOfImages }: any = project;
 
   return (
-    <div className='container px-4 mx-auto mb-12'>
-      <h1 className="text-5xl font-black mb-4">Gallery</h1>
+    <div className='container mx-auto mb-12 px-4'>
+      <h1 className="mb-4 text-5xl font-black">Gallery</h1>
       <div className="grid gap-4 lg:grid-cols-2 2xl:grid-cols-3">
         {ListOfImages.map((image: string, index: number) => (
           <Image
             src={`/image/${image}`}
             height="1000"
             width="1000"
-            className="object-cover rounded-xl my-2 shadow-md xl:hover:scale-105 xl:hover:shadow-lg xl:hover:shadow-neutral-500 transition-all duration-300 ease-in"
+            className="my-2 rounded-xl border object-cover shadow-md transition-all duration-300 ease-in xl:hover:scale-105"
             alt={'image'}
             key={index}
           />
