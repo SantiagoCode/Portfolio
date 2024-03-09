25fcb508f7a2e26c7942fc979470025d79966f4d
Santiago Salazar <santiagosalazar.dev@gmail.com>
2024-03-09 02:07:25 -0400

    New 3D Banner of skills

--- src/partials/Hero.tsx
+++ src/partials/Hero.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 import { TextGenerateEffect } from '@/components/ui/text-generate-effect'
 import FixedBackground from '@/components/FixedBackground'
+import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 
 const Hero = () => {
   const logoTechnologies = [
@@ -20,22 +21,32 @@ const Hero = () => {
         <TextGenerateEffect words={"Santiago Salazar"} className='mb-0 text-center text-7xl font-black' />
         <TextGenerateEffect words={"FrontEnd Developer"} className='text-center text-3xl font-[300]' />
       </h1>
-      
-      <ul className='z-20 flex rounded-full border bg-[rgb(255,255,255,0.1)] px-8 py-2 backdrop-blur-lg' data-aos="zoom-in-up" data-aos-delay="900" data-aos-duration="900">
-        {logoTechnologies.map(([text, ext, w, h], index) => (
-          <li key={index} className='flex items-center'>
-            <Image
-              src={`/image/${text}.${ext}`}
-              alt="next Logo"
-              className="rounded transition-all xl:hover:scale-105"
-              width={Number(w)}
-              height={Number(h)}
-              priority
-            />
-            {( index < logoTechnologies.length - 1 ) && <div className='mx-4 h-4 w-px bg-white'></div>  }
-          </li>
-        ))}
-      </ul>
+
+      <CardContainer className="inter-var flex before:absolute before:h-full before:w-full before:rounded-full before:bg-[rgb(255,255,255,0.1)] before:backdrop-blur-xl before:content-['']">
+        <CardBody className="relative h-full w-auto px-8 py-2">
+          <ul className='flex items-center'>
+            {logoTechnologies.map(([text, ext, w, h], index) => (
+              <CardItem
+                translateZ={40}
+                className="rounded-xs px-2"
+                key={`${index}-stack`}
+              >
+                <li key={index} className='flex items-center'>
+                <Image
+                  src={`/image/${text}.${ext}`}
+                  alt="next Logo"
+                  className="rounded transition-all xl:hover:scale-105"
+                  width={Number(w)}
+                  height={Number(h)}
+                  priority
+                />
+                {( index < logoTechnologies.length - 1 ) && <div className='mx-4 h-4 w-px bg-white'></div>  }
+              </li>
+              </CardItem>
+              ))}
+          </ul>
+        </CardBody>
+      </CardContainer>
     </div>
   )
 }
