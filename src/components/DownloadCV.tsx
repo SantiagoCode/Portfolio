3118bea73e9846bcf700c00bd8f8ef04b6bf6c94
Santiago Salazar <santiagosalazar.dev@gmail.com>
2024-03-13 13:07:06 -0400

    Download CV button fix

--- src/components/DownloadCV.tsx
+++ src/components/DownloadCV.tsx
@@ -6,13 +6,13 @@ const DownloadCV = () => {
   return (
     <CardBody className='w-[fit-content]'>
       <CardContainer>
+        <Link href="CV/Santiago_Salazar-FrontEnd_Dev.pdf" target="_blank" download>
           <CardItem
             translateZ="20"
             className='flex bg-[rgb(255,255,255,0.1)] hover:bg-[rgb(255,255,255,0.2)] transition-all backdrop-blur-xl border rounded-full py-3 px-6'>
-            <Link href="CV/Santiago_Salazar-FrontEnd_Dev.pdf" target="_blank" download>
-                    Descargar CV
-            </Link>
+              Download CV
           </CardItem>
+        </Link>
       </CardContainer>
     </CardBody>
   )
