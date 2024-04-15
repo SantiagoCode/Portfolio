6b765fe2f7eb7e244d8b2aeaab14b9b5f1ca3545
Santiago Salazar <santiagosalazar.dev@gmail.com>
2024-04-14 21:28:27 -0400

    navigation width

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -16,8 +16,8 @@ const Navigation = () => {
 	return (
 		<div id='Navigation' className='fixed z-40 w-full opacity-0 transition-all delay-500 duration-500'>
 			<div className='container mx-auto px-4'>
-				<nav className='mx-auto mt-5 flex justify-between rounded-full border bg-[rgb(255,255,255,0.3)] px-8 backdrop-blur-lg'>
-					<div className='flex'>
+				<nav className='mx-auto mt-5 flex w-11/12 justify-between rounded-full border bg-[rgb(255,255,255,0.3)] px-8 backdrop-blur-lg'>
+					<div className='flex transition-all hover:scale-110'>
 						<Link href='/'>
 							<Image src='/image/logo_santiago1.svg' alt='logo' className='h-12' width={120} height={70} />
 						</Link>
