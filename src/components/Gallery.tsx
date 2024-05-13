ebf5623d145564202816877d8a2a964bc6183280
Santiago Salazar <santiagosalazar.dev@gmail.com>
2024-05-13 08:02:49 -0400

    chore: optimize Gallery component for better performance

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
 
 const Gallery = ({ project }: { project: any }) => {
-  const { ListOfImages }: any = project;
+	const { ListOfImages }: any = project;
 
-  return (
-    <div id='Gallery' className='container z-10 mx-auto mb-12 px-4 opacity-0 transition-all duration-500'>
-      <h1 className="mb-4 text-5xl font-black">Gallery</h1>
-      <div className="grid gap-4 lg:grid-cols-2 2xl:grid-cols-3">
-        {ListOfImages.map((image: string, index: number) => (
-          <Image
-            src={`/image/${image}`}
-            height="1000"
-            width="1000"
-            className="my-2 rounded-xl border object-cover shadow-md transition-all duration-300 ease-in xl:hover:scale-105"
-            alt={'image'}
-            key={index}
-          />
-        ))}
-      </div>
-    </div>
-  )
-}
+	return (
+		<div id='Gallery' className='container z-10 mx-auto mb-12 px-4 opacity-0 transition-all duration-500'>
+			<h1 className='mb-4 text-5xl font-black'>Gallery</h1>
+			<div className='grid gap-4 lg:grid-cols-2'>
+				{ListOfImages.map((image: string, index: number) => (
+					<Image
+						src={`/image/${image}`}
+						height='1000'
+						width='1000'
+						className='my-2 rounded-xl border object-cover shadow-md transition-all duration-300 ease-in xl:hover:scale-105'
+						alt={'image'}
+						key={index}
+					/>
+				))}
+			</div>
+		</div>
+	);
+};
 
-export default Gallery
+export default Gallery;
